1ddfa9c3033bc07f10dad5a67a35470181e1063e
Baraka M Mulumia <65751779+iambaraka@users.noreply.github.com>
2022-12-02 23:29:16 +0300

    Create boilerplate for Projects Section

--- src/portfolio/projects/index.tsx
+++ src/portfolio/projects/index.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Box, Stack, Text, VStack } from "@chakra-ui/react";
+
+import SectionHeader from "../../components/SectionHeader";
 
 const ProjectsSection = () => {
-  return <div>ProjectsSection</div>;
+  return (
+    <Stack
+      w={"full"}
+      pt={32}
+      h={"100vh"}
+      bg={"gray.800"}
+      spacing={20}
+      justifyContent={"space-between"}
+    >
+      <Stack spacing={10} w={"full"}>
+        <Box w={"full"} pos={"relative"}>
+          <SectionHeader title={"Projects"} subtitle={"Show Case"} />
+        </Box>
+        <VStack pt={20}>
+          <Text color={"gray.400"} size={"xl"}>
+            ... section coming Soon ...
+          </Text>
+        </VStack>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default ProjectsSection;
