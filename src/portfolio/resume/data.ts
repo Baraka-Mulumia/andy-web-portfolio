d2dcea1cd6e81c2f24e6518b8c83afecc1fb85d4
Baraka M Mulumia <65751779+iambaraka@users.noreply.github.com>
2022-12-02 23:28:05 +0300

    Create data.ts

---
+++ src/portfolio/resume/data.ts
@@ -0,0 +1,133 @@
+export const skills = [
+  {
+    name: "ISO 27001 Implementation",
+    proficiency: 90,
+  },
+  {
+    name: "Firewall Configuration (Juniper SRX & Fortigate)",
+    proficiency: 90,
+  },
+  {
+    name: "Vulnerability Management - Tenable",
+    proficiency: 90,
+  },
+  {
+    name: "Network Monitoring - Solarwinds",
+    proficiency: 90,
+  },
+  {
+    name: "Anti Virus - Symantec",
+    proficiency: 80,
+  },
+  {
+    name: "Network Security - Cisco ASA",
+    proficiency: 80,
+  },
+  {
+    name: "Web Technologies - (HTML, CSS, JavaScript)",
+    proficiency: 70,
+  },
+  {
+    name: "Programming - (Python)",
+    proficiency: 70,
+  },
+  {
+    name: "Bash Scripting",
+    proficiency: 80,
+  },
+  {
+    name: "Project Management",
+    proficiency: 90,
+  },
+];
+
+export const experiences = [
+  {
+    title: "Senior Security Analyst",
+    company: "Citi",
+    companyLink: "https://www.citi.com/",
+    timeline: "2022 - Present",
+    description:
+      "Working as a Senior Security Analyst in Citi's Global Security Operations Center (GSOC).",
+    responsibilities: [
+      "Performing security monitoring and incident response for Citi's global network.",
+      "Performing security monitoring and incident response for Citi's global network.",
+    ],
+  },
+  {
+    title: "Cyber Security Technical Trainer",
+    company: "Institute of Software Technologies",
+    companyLink: "https://isteducation.com/",
+    timeline: "May 2021 - Dec 2022",
+    description: "Working as a Cyber Security Technical Trainer at IST.",
+    responsibilities: [
+      "Teaching Cyber Security courses to students.",
+      "Supervising students' projects.",
+    ],
+  },
+  {
+    title: "Technical Support Engineer",
+    company: "CONSULTIX",
+    companyLink: "https://consultixwireless.com/",
+    timeline: "Sept 2020 - May 2021",
+    description:
+      "Consultix has the most comprehensive portfolio in the industry for IBS / DAS testing and design tools. Consultix unique solutions cover the entire IBS/DAS project life cycle end to end; From design, verification, installation testing and commissioning all the way to long-term maintenance, optimization and monitoring solutions. And the below are my responsibilities in the company.",
+    responsibilities: [
+      "Firmware upgrades",
+      "Providing technical support to customers",
+      "1st and 2nd line support",
+      "Troubleshooting",
+      "Technical documentation",
+    ],
+  },
+];
+
+export const education = [
+  {
+    courseName: "BSc. Computer forensics and Cyber Security",
+    institute: "University of Nairobi",
+    instituteLink: "https://www.uonbi.ac.ke/",
+    timeline: "2015 - 2019",
+  },
+  {
+    courseName: "Diploma in Cyber Security",
+    institute: "Institute of Software Technologies",
+    instituteLink: "https://isteducation.com/",
+    timeline: "2020 - 2021",
+    coursesIncluded: [
+      "Usable security",
+      "Software security",
+      "Cryptography",
+      "Penetration testing",
+    ],
+  },
+];
+
+export const certifications = [
+  {
+    timeline: "2021",
+    courseName: "CompTIA Security+",
+    institute: "Udacity NanoDegree diploma details",
+    instituteLink: "https://www.udacity.com/course/comptia-security--ud611",
+  },
+  {
+    timeline: "2021",
+    courseName: "AWS Developer Associate",
+    institute: "Udacity NanoDegree diploma details",
+    instituteLink:
+      "https://www.udacity.com/course/aws-developer-associate-nanodegree--nd9991",
+  },
+  {
+    timeline: "2021",
+    courseName: "PECB Certified ISO/IEC 27001 Auditor",
+    institute: "PECB",
+    instituteLink:
+      "https://www.pecb.com/en/certification/iso-iec-27001-auditor",
+  },
+  {
+    timeline: "May 2020 - Nov 2020",
+    courseName: "JNCIS-SEC - Cyber Security Specialist",
+    institute: "Juniper Networks",
+    instituteLink: "https://www.juniper.net/us/en/training/certification/",
+  },
+];
