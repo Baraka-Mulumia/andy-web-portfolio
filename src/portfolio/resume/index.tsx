1b1ddfe13e7e34bfbe64a8beb6ed5a4de252c6b9
Baraka M Mulumia <65751779+iambaraka@users.noreply.github.com>
2022-12-02 23:28:56 +0300

    Feat  :sparkles:  Mile stone - Resume Section

--- src/portfolio/resume/index.tsx
+++ src/portfolio/resume/index.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { Box, Divider, Stack } from "@chakra-ui/react";
+
+import Education from "./Education";
+import Experience from "./Experience";
+import SectionHeader from "../../components/SectionHeader";
+import Skills from "./Skills";
 
 const ResumeSection = () => {
-  return <div>ResumeSection</div>;
+  return (
+    <Stack
+      w={"full"}
+      pt={32}
+      pb={8}
+      bg={"#212529"}
+      spacing={20}
+      justifyContent={"space-between"}
+    >
+      <Stack spacing={10} w={"full"} px={2}>
+        <Box w={"full"} pos={"relative"}>
+          <SectionHeader title={"Summary"} subtitle={"Resume"} />
+        </Box>
+        <Stack
+          spacing={10}
+          w={"full"}
+          px={2}
+          placeSelf={"center"}
+          maxW={"container.lg"}
+          direction={{ base: "column", md: "row" }}
+          placeContent={{
+            base: "center",
+            md: "space-evenly",
+          }}
+          placeItems={{
+            base: "center",
+            md: "space-evenly",
+          }}
+        >
+          <Experience />
+          <Education />
+        </Stack>
+        <Divider />
+        <Skills />
+      </Stack>
+    </Stack>
+  );
 };
 
 export default ResumeSection;
