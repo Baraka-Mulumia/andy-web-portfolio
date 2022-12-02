ac742122eb812c18cc2f9e7db9abf7c3bf1ddb54
Baraka M Mulumia <65751779+iambaraka@users.noreply.github.com>
2022-12-02 23:26:30 +0300

    Update Contact Section

--- src/portfolio/contact/index.tsx
+++ src/portfolio/contact/index.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { Box, Link, Stack, Text } from "@chakra-ui/react";
+
+import ContactDetails from "./ContactDetails";
+import ContactForm from "./ContactForm";
+import SectionHeader from "../../components/SectionHeader";
 
 const ContactSection = () => {
-  return <div>ContactSection</div>;
+  return (
+    <Stack
+      w={"full"}
+      pt={32}
+      bg={"rgba(0,0,0,0.7)"}
+      spacing={20}
+      justifyContent={"space-between"}
+    >
+      <Stack spacing={10} w={"full"}>
+        <Box w={"full"} pos={"relative"}>
+          <SectionHeader title={"Contact"} subtitle={"Get in Touch"} />
+        </Box>
+        <Stack
+          direction={{ base: "column", md: "row" }}
+          spacing={{ base: 10, md: 20 }}
+          placeItems={{ base: "center", md: "flex-start" }}
+          placeContent={{ base: "center" }}
+          pt={10}
+        >
+          <ContactDetails />
+          <ContactForm />
+        </Stack>
+      </Stack>
+      <Box w={"full"} p={10} bg={"gray.900"}>
+        <Text color={"white"} textAlign={"center"}>
+          Made with ❤️{" "}
+          <Link href="https://iambaraka.dev" isExternal>
+            <Text as={"span"} color={"#FFD700"}>
+              iambaraka.dev{" "}
+            </Text>
+          </Link>
+          © {new Date().getFullYear()}
+        </Text>
+      </Box>
+    </Stack>
+  );
 };
 
 export default ContactSection;
