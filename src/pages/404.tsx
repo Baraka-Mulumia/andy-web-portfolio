a82eeba6571cf174214ca1cb5422d9608bf0bab9
Baraka M Mulumia <65751779+iambaraka@users.noreply.github.com>
2022-12-05 14:54:39 +0300

    Create 404.tsx
    fixes  #1

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+import PageNotFound from "../components/PageNotFound";
+import React from "react";
+
+const NoMatch: NextPage = () => <PageNotFound />;
+
+export default NoMatch;
